46f662f0235177510bf07c8ae70b1d3e094898fe
nicolas <nicolas.lidin@gmail.com>
2025-06-29 17:34:12 +0200

    feat: push nuxt and strapi

---
+++ NuxtStarterKit/src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  qgd
+  <MarkdownText content-md="ddqdld" />
+</template>
+
+<script setup lang="ts">
+
+import {MarkdownText} from "vue3-vite-vuetify-lib-starter-kit";
+
+const { t, locale } = useI18n();
+
+import { ref } from "vue";
+const val = ref(null);
+
+const config = useRuntimeConfig();
+
+
+onMounted(() => {
+});
+</script>
+
+<style scoped lang="scss"></style>
