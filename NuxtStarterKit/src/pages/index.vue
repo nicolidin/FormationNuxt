25c6cf6906b569be6d6450f4d40f3f7f0e731e1f
nicolas <nicolas.lidin@gmail.com>
2025-06-29 18:58:51 +0200

    feat: add sufix api to strapi env

--- NuxtStarterKit/src/pages/index.vue
+++ NuxtStarterKit/src/pages/index.vue
@@ -1,19 +1,8 @@
 <template>
-  qgd
-  <MarkdownText content-md="ddqdld" />
+  Hey it's home
 </template>
 
 <script setup lang="ts">
-
-import {MarkdownText} from "vue3-vite-vuetify-lib-starter-kit";
-
-const { t, locale } = useI18n();
-
-import { ref } from "vue";
-const val = ref(null);
-
-const config = useRuntimeConfig();
-
 
 onMounted(() => {
 });
